9dd01fb1e4fb69fa95c82d1c783c5e9a329ca5fe
kunalthakur29991-eng <kunalthakur29991@gmail.com>
2025-08-26 15:10:53 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int  main()
+{
+     int a,b, area;
+      printf("enter the value of length and breath");
+      scanf("%d" "%d",a, b);
+   area= a*b;
+   printf("%d",area);
+   getch();
+   return 0;
+}
