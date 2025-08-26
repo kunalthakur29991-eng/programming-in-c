49f0922eb25c6283ff785e2877c1afbae6b420ca
kunalthakur29991-eng <kunalthakur29991@gmail.com>
2025-08-26 15:19:40 +0530

    Add files via upload

---
+++ 2 numbers .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the first number a");
+    scanf("%d",&a);
+    printf("enter the first number b");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("%d%",sum);
+    getch();
+    return 0;
+    }
+    
