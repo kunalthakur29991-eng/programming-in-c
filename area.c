9b0b7498df4994d6b4381670150bae1e58cf7023
kunalthakur29991-eng <kunalthakur29991@gmail.com>
2025-08-31 23:51:31 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,area ;
+    printf("a is length of ractangle");
+    scanf("%d",a);
+    printf("b is breadth of ractangle");
+    scanf("%d",b);
+    area=a*b;
+    printf("%d%",area);
+    return 0;
+}
